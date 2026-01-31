5cf00f110d2bf3061534a8b5380f575df9a406a1
AntonosSamaan <tonisamaan5@gmail.com>
2026-01-31 13:09:41 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-require_once '../inc/functions.php';
-require_login();
-session_destroy();
-session_start();
-$_SESSION['success']="User loguot success";
-redirect('login.php');
-?>
